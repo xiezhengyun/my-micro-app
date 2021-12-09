a01df79910a450c6cc7a59870f029f39f85ed750
xiezy <xiezy@smartebao.com>
2021-12-09 15:34:42 +0800

    fix: test

--- src/subApp.js
+++ src/subApp.js
@@ -1,7 +1,7 @@
 window.name = 'subapp'
 window.sub = 'sub'
 var sub2 = 'sub2'
-console.log(name, sub, sub2)
+console.log(window, name, sub, sub2)
 
 window.addEventListener('click',()=>{
   console.log('subapp click' + name)
